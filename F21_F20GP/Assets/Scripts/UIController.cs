2cf750574ef6f85b76a4d01652c1b9900340bcd5
tehdeadpixel <50940928+tehdeadpixel@users.noreply.github.com>
2023-03-27 16:49:43 +0100

    Player and gun logic

---
+++ F21_F20GP/Assets/Scripts/UIController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+    public Text ammoText;
+
+    private void Awake()
+    {
+        instance = this; 
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
